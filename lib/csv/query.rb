46bf2a1492b00c525e6a4bbc5f2bb1f865d32ae9
naofumi-fujii <naofumi.fujii@gmail.com>
2018-05-29 10:25:44 +0900

    いろいろかえた

--- lib/csv/query.rb
+++ lib/csv/query.rb
@@ -2,52 +2,55 @@ require "csv/query/version"
 require 'csv'
 require 'sqlite3'
 require 'active_record'
+require 'optparse'
 
 module Csv
   module Query
     # Your code goes here...
     def self.run!
-      InMemoryAR.new.run! { pp InMemoryAR::Record.all }
+      file_path =
+        ARGV.getopts(nil, 'file:').values[0]
+
+      InMemoryAR.new(file_path).run! { pp InMemoryAR::Record.all }
     end
 
     class InMemoryAR
       class Record < ActiveRecord::Base
       end
 
-      class CsvControl
-        class << self
-          def file_path
-            '/Users/s01001/Downloads/head10.csv'
-          end
-
-          # `LP(iphone)`みたいに
-          # 半角カッコ内にアルファベットだと
-          # Rubyのsyntax的にsetterと勘違いされるので対策
-          def header_converter
-            lambda do |h|
-              h.gsub('(','（').gsub(')', '）')
-            end
-          end
-
-          def csv
-            CSV.read(file_path, encoding: "SJIS:UTF-8", headers: true, header_converters: header_converter)
-          end
-
-          def csv_headers
-            csv.headers
-          end
+      # `LP(iphone)`みたいに
+      # 半角カッコ内にアルファベットだと
+      # Rubyのsyntax的にsetterと勘違いされるので対策
+      def header_converter
+        lambda do |h|
+          h.gsub('(','（').gsub(')', '）')
         end
       end
 
+      def csv
+        CSV.read(file_path, encoding: "SJIS:UTF-8", headers: true, header_converters: header_converter)
+      end
 
-      def initialize
+      def csv_headers
+        csv.headers
+      end
+
+      attr_reader :file_path
+
+      def initialize file_path
+        @file_path = file_path
+
+        migration(csv_headers)
+      end
+
+      def migration csv_headers
         ActiveRecord::Migration.verbose = false
 
         ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 
         ActiveRecord::Schema.define(:version => 1) do
           create_table :records do |t|
-            CsvControl.csv_headers.map do |column_name|
+            csv_headers.map do |column_name|
               t.text column_name.to_sym
             end
           end
@@ -55,7 +58,7 @@ module Csv
       end
 
       def run!
-        CsvControl.csv.each do |row|
+        csv.each do |row|
           Record.create!(row.to_h)
         end
         yield
